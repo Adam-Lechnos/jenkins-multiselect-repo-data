cd59bfbd28551f5e2cadedf13bf0758faa7839e2
Adam M. Lechnos <adam.lechnos@gmail.com>
2022-02-28 15:06:12 -0500

    Update multi_selector_json.sh

--- multi_selector_json.sh
+++ multi_selector_json.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+source 'add auth.cfg file here'
+
 # add token here
-authToken=
+authToken=$gitauthtoken
 repoName=$1
 repoRelease=$2
 repoNameTrim=`echo $repoName | cut -d':' -f2 | cut -d'/' -f2 | sed 's/.git//g'`
 repoOwner=`echo $repoName | cut -d':' -f2 | cut -d'/' -f1`
+logDir='add log directory here'
 
 payload='query RepoFiles{
 repository(owner: \"'$repoOwner'\", name: \"'$repoNameTrim'\") {
@@ -54,24 +57,24 @@ payloadFrmt="$(echo $payload)"
 
 
 logretention=20
-loglist=$(ls -l /var/lib/jenkins/git/cloud-market-data/jenkins_tools/multiselect_json_create/logs/ | grep .log | awk '{print $9}' | wc -l)
+loglist=$(ls -l $logDir/multiselect_json_create/logs/ | grep .log | awk '{print $9}' | wc -l)
 
-if [ ! -d /var/lib/jenkins/git/cloud-market-data/jenkins_tools/multiselect_json_create/logs ]; then
-  mkdir /var/lib/jenkins/git/cloud-market-data/jenkins_tools/multiselect_json_create/logs
+if [ ! -d $logDir/multiselect_json_create/logs ]; then
+  mkdir $logDir/multiselect_json_create/logs
 fi
 
 if [ $loglist -gt $logretention ]
 then
 
  delcount=`expr $loglist - $logretention`
- find /var/lib/jenkins/git/cloud-market-data/jenkins_tools/multiselect_json_create/logs/ -type f -printf '%T+ %p\n' | sort | head -n $delcount | awk '{print $2}' | sed 's/[^\]*logs[^\]//' | xargs -I {} rm /var/lib/jenkins/git/cloud-market-data/jenkins_tools/multiselect_json_create/logs/{}
+ find $logDir/multiselect_json_create/logs/ -type f -printf '%T+ %p\n' | sort | head -n $delcount | awk '{print $2}' | sed 's/[^\]*logs[^\]//' | xargs -I {} rm $logDir/multiselect_json_create/logs/{}
 
 fi
 
 
 curl \
 -s -H "Authorization: bearer $authToken" \
--X POST -d "{ \"query\": \"$payloadFrmt\"}" https://api.github.factset.com/graphql \
+-X POST -d "{ \"query\": \"$payloadFrmt\"}" https://api.github.com/graphql \
 | jq -r -e '.data.repository.object.entries[].object.entries|.[]?|.object.entries|.[]?|.name'
 
 exitStatus=$?
@@ -83,7 +86,7 @@ then
 
  curl -s -I \
  -H "Authorization: bearer $authToken" \
- https://api.github.factset.com/graphql > /var/lib/jenkins/git/cloud-market-data/jenkins_tools/multiselect_json_create/logs/cURLheader_$timestamp.log
+ https://api.github.factset.com/graphql > $logDir/multiselect_json_create/logs/cURLheader_$timestamp.log
 
 else
 
